4248bf1aef2ee6b3165eb96282566718237a3cae
Oleg  Shuliak <nformals@gmail.com>
2024-05-07 03:35:23 +0300

    add report v2.2

--- report.c
+++ report.c
@@ -13,35 +13,43 @@ int main(int argc, char* argv[]) {
 	char* name;
 	int monthNum,  month;
 	
-	while ((rez = getopt (argc, argv, "hf:m:u")) != -1) {
-		switch(rez) {
-			case 'h':
+	if (!argv[1]){
 				printf("Temp statistic application. Please enter key:\n");
 				printf("-h for help.\n");
 				printf("-f file_name for load this file.\n");
 				printf("-m xx statistic for xx month.\n");
-				break;
-			case 'f':
-				name = optarg;
-				monthNum = ReadCSV(mStat, name);
-				if (!argv[3]){
-					yStat = AddYearStat(yStat, mStat, monthNum);
-					printAllMonth(mStat, monthNum);
-					printf("In %d avg = %f, min = %d, max = %d\n",mStat[0].year, yStat.yearAvg, yStat.yearMin, yStat.yearMax);
-				break;
-				}
-				else {
-					case 'm':
-						month = atoi (optarg);
-						printMonth(mStat, monthNum, month);
-						break;
-				}
-			default:
-				printf("Temp statistic application. Please enter key:\n");
-				printf("-h for help.\n");
-				printf("-f file_name for load this file.\n");
-				printf("-m xx statistic for xx month.\n");
-				break;
+			}
+	else {
+		while ((rez = getopt (argc, argv, "hf:m:u")) != -1) {
+			switch(rez) {
+				case 'h':
+					printf("Temp statistic application. Please enter key:\n");
+					printf("-h for help.\n");
+					printf("-f file_name for load this file.\n");
+					printf("-m xx statistic for xx month.\n");
+					break;
+				case 'f':
+					name = optarg;
+					monthNum = ReadCSV(mStat, name);
+					if (!argv[3]){
+						yStat = AddYearStat(yStat, mStat, monthNum);
+						printAllMonth(mStat, monthNum);
+						printf("In %d avg = %f, min = %d, max = %d\n",mStat[0].year, yStat.yearAvg, yStat.yearMin, yStat.yearMax);
+					break;
+					}
+					else {
+						case 'm':
+							month = atoi (optarg);
+							printMonth(mStat, monthNum, month);
+							break;
+					}
+				default:
+					printf("Temp statistic application. Please enter key:\n");
+					printf("-h for help.\n");
+					printf("-f file_name for load this file.\n");
+					printf("-m xx statistic for xx month.\n");
+					break;
+			}
 		}
 	}
 return 0;
